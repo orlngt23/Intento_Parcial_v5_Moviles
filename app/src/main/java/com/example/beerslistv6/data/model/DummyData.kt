1d41d7a930e7da755d78ff46d9880d9d4c4138ea
orlngt23 <00029221@uca.edu.sv>
2023-06-06 17:37:25 -0600

    Creacion de el paquete Model

--- app/src/main/java/com/example/beerslistv6/data/model/DummyData.kt
+++ app/src/main/java/com/example/beerslistv6/data/model/DummyData.kt
@@ -1,2 +1,15 @@
 package com.example.beerslistv6.data.model
 
+//beer 01
+const val name = "Corona"
+const val type = "Pilsner"
+
+//beer 02
+
+const val name2 = "Heineken"
+const val type2 = "Pilsen "
+
+var beers = mutableListOf(
+    BeerModel(name, type),
+    BeerModel(name2, type2)
+)
